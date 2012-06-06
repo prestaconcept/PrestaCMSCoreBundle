a5db73852e8ff2cf3dbe527d022f6d3f7449e83e
Nicolas Bastien <nbastien@prestaconcept.net>
2012-06-06 09:34:44 +0200

    remove old comment

--- Entity/BaseThemeBlock.php
+++ Entity/BaseThemeBlock.php
@@ -37,26 +37,6 @@ abstract class BaseThemeBlock extends BaseBlock
      */
     protected $zone;
 
-//    /**
-//     * @var integer $position
-//     */
-//    protected $position;
-
-//    /**
-//     * @var boolean $is_active
-//     */
-//    protected $is_active;
-
-//    /**
-//     * @var string
-//     */
-//    protected $type;
-//
-//    /**
-//     * @var text
-//     */
-//    protected $settings;
-
     /**
      * Used locale to override Translation listener`s locale
      * this is not a mapped field of entity metadata, just a simple property
@@ -140,86 +120,4 @@ abstract class BaseThemeBlock extends BaseBlock
     {
         return $this->zone;
     }
-
-//    /**
-//     * Set position
-//     *
-//     * @param integer $position
-//     * @return BaseThemeBlock
-//     */
-//    public function setPosition($position)
-//    {
-//        $this->position = $position;
-//        return $this;
-//    }
-//
-//    /**
-//     * Get position
-//     *
-//     * @return integer 
-//     */
-//    public function getPosition()
-//    {
-//        return $this->position;
-//    }
-
-    
-
-//    /**
-//     * Set block_type
-//     *
-//     * @param string $type
-//     * @return BaseThemeBlock
-//     */
-//    public function setType($type)
-//    {
-//        $this->type = $type;
-//        return $this;
-//    }
-//
-//    /**
-//     * Get block_type
-//     *
-//     * @return string 
-//     */
-//    public function getType()
-//    {
-//        return $this->type;
-//    }
-
-//    /**
-//     * Set content
-//     *
-//     * @param text $content
-//     * @return BaseThemeBlock
-//     */
-//    public function setSettings(array $settings = array())
-//    {
-//        $this->settings = $settings;
-//        return $this;
-//    }
-//
-//    /**
-//     * Get content
-//     *
-//     * @return text 
-//     */
-//    public function getSettings()
-//    {
-//        if (!is_array($this->settings)) {
-//            return array($this->settings);
-//        }
-//        return $this->settings;
-//    }
-//    
-//    public function prePersist()
-//    {var_dump('persist!');die;
-//        $this->settings = serialize($this->settings);
-//        var_dump($this);die;
-//    }
-//    
-//    public function postLoad()
-//    {
-//        $this->settings = unserialize($this->settings);
-//    }
 }
