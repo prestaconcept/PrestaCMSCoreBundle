085e70f5a414afef2318c95e65a307b1daf9fa8a
Pierre-Yves Jamon <pjamon@prestaconcept.net>
2012-07-06 11:37:26 +0200

    added CMS admin menu translations

--- EventListener/ConfigureMenuListener.php
+++ EventListener/ConfigureMenuListener.php
@@ -21,33 +21,44 @@ use PrestaSonata\AdminBundle\Event\ConfigureMenuEvent;
 class ConfigureMenuListener
 {
     /**
+     * @var Symfony\Component\DependencyInjection\Container
+     */
+    protected $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
      * @param \PrestaSonata\AdminBundle\Event\ConfigureMenuEvent $event
      */
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
+        $translator = $this->container->get('translator');
         $menu = $event->getMenu();
 
         $cmsMenuItem = $menu->addChild('CMS', array('route' => 'presta_cms_website'));
         $cmsMenuItem->setAttribute('class', 'dropdown');
-        $cmsMenuItem->setLabel('CMS <span class="caret"></span>');
+        $cmsMenuItem->setLabel($translator->trans('admin.cms').'<span class="caret"></span>');
         $cmsMenuItem->setExtra('safe_label', true);
         $cmsMenuItem->setLinkAttribute('class', 'dropdown-toggle  nav-menu nav-menu-cms');
         $cmsMenuItem->setLinkAttribute('data-toggle', 'dropdown');
         $cmsMenuItem->setChildrenAttribute('class', 'dropdown-menu');
-                
+
         $websiteItem = $cmsMenuItem->addChild('Websites', array('route' => 'presta_cms_website'));
 		$websiteItem->setExtra('safe_label', true);
-		$websiteItem->setLabel('Websites <p>Lorem ipsum websites</p>');
+		$websiteItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.websites'), $translator->trans('admin.websites.description')));
 		$websiteItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-websites');
 
 		$pageItem = $cmsMenuItem->addChild('Pages', array('route' => 'presta_cms_page'));
 		$pageItem->setExtra('safe_label', true);
-		$pageItem->setLabel('Pages <p>Lorem ipsum pages</p>');
+        $pageItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.pages'), $translator->trans('admin.pages.description')));
 		$pageItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-pages');
 
         $themeItem = $cmsMenuItem->addChild('Themes', array('route' => 'presta_cms_theme'));
 		$themeItem->setExtra('safe_label', true);
-		$themeItem->setLabel('Themes <p>Lorem ipsum themes</p>');
+        $themeItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.themes'), $translator->trans('admin.themes.description')));
 		$themeItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-themes');
     }
 }
