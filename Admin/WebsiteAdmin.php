02e98de69d95703c790338d8deea24332e9b69bb
Nicolas Bastien <nbastien@prestaconcept.net>
2012-11-28 17:48:08 +0100

    Refactoring websites in document

--- Admin/WebsiteAdmin.php
+++ Admin/WebsiteAdmin.php
@@ -13,7 +13,7 @@ namespace Presta\CMSCoreBundle\Admin;
 use Knp\Menu\ItemInterface as MenuItemInterface;
 use Knp\Menu\MenuItem;
 
-use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Form\FormMapper;
@@ -21,15 +21,17 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Presta\CMSCoreBundle\Model\ThemeManager;
 
+//use Presta\SonataAdminBundle\Admin\BaseAdmin;
+use Presta\SonataAdminBundle\Admin\PHPCR\BaseAdmin;
 
 /**
  * Admin definition for the Site class
  *
- * @package    PrestaCMS
- * @subpackage CoreBundle
+ * @package    Presta
+ * @subpackage CMSCoreBundle
  * @author     Nicolas Bastien <nbastien@prestaconcept.net>
  */
-class WebsiteAdmin extends Admin
+class WebsiteAdmin extends BaseAdmin
 {
     /**
      * @var array
@@ -41,12 +43,12 @@ class WebsiteAdmin extends Admin
      * @var Presta\CMSCoreBundle\Model\ThemeManager
      */
     protected $_themeManager;
-    
+
 
     /**
      * Set available locales : called via DI
-     * 
-     * @param array $availableLocales 
+     *
+     * @param array $availableLocales
      */
     public function setAvailableLocales($availableLocales)
     {
@@ -56,8 +58,8 @@ class WebsiteAdmin extends Admin
 
     /**
      * Setter for _themeManager
-     * 
-     * @param   ThemeManager $themeManager 
+     *
+     * @param   ThemeManager $themeManager
      * @return  void
      */
     public function setThemeManager(ThemeManager $themeManager)
@@ -65,24 +67,30 @@ class WebsiteAdmin extends Admin
         $this->_themeManager = $themeManager;
     }
 
-
     /**
-     * Add locale param to edit url
-     * 
-     * @param   $name 
-     * @param   array $parameters 
-     * @param   bool $absolute
-     * @return  string
+     * {@inheritdoc}
      */
-    public function generateUrl($name, array $parameters = array(), $absolute = false)
+    protected function configureListFields(ListMapper $listMapper)
     {
-        if (!isset($parameters['locale']) && $this->hasSubject()) {
-            $parameters['locale'] = $this->getSubject()->getLocale();
-        }
+        $listMapper
+            ->addIdentifier('name', 'text')
+            ->add('host', 'text')
+            ->add('theme', 'text')
+            ->add('defaultLocale', 'locale', array('template' => 'PrestaSonataAdminBundle:CRUD:list_locale.html.twig'))
+            ->add('availableLocales', 'array', array('template' => 'PrestaSonataAdminBundle:CRUD:list_array_locale.html.twig'))
 
-        return $this->routeGenerator->generateUrl($this, $name, $parameters, $absolute);
+            ->add('isActive', 'boolean')
+            ->add('isDefault', 'boolean')
+
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'view' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
     }
-
 
     /**
      * {@inheritdoc}
@@ -93,61 +101,71 @@ class WebsiteAdmin extends Admin
             ->add('name', null, array('label' => 'admin.name'))
             ->add('host', null, array('label' => 'admin.host'))
             ->add('isDefault', 'boolean', array('label' => 'admin.isDefault'))
-            ->add('isActive', 'boolean', array('label' => 'admin.isActive'))    
+            ->add('isActive', 'boolean', array('label' => 'admin.isActive'))
             ->add('defaultLocale', null, array('label' => 'admin.defaultLocale'))
         ;
     }
-    
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->addIdentifier('name', null, array('label' => 'admin.name'))
-            ->add('defaultLocale', 'locale', array('label' => 'admin.defaultLocale', 'template' => 'PrestaSonataAdminBundle:CRUD:list_locale.html.twig'))
-            ->add('availableLocales', 'array', array('label' => 'admin.availableLocales', 'template' => 'PrestaSonataAdminBundle:CRUD:list_array_locale.html.twig'))
-            ->add('isActive', 'boolean', array('label' => 'admin.isActive'))
-            ->add('isDefault', 'boolean', array('label' => 'admin.isDefault'))
-        ;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        //SF forms update hasn't been changed in sonata yet !
-//        $datagridMapper
-//            ->add('name')
-//            ->add('host')
-//        ;
-    }
-
-
+//
+//
+//    /**
+//     * Add locale param to edit url
+//     *
+//     * @param   $name
+//     * @param   array $parameters
+//     * @param   bool $absolute
+//     * @return  string
+//     */
+//    public function generateUrl($name, array $parameters = array(), $absolute = false)
+//    {
+//        if (!isset($parameters['locale']) && $this->hasSubject()) {
+//            $parameters['locale'] = $this->getSubject()->getLocale();
+//        }
+//
+//        return $this->routeGenerator->generateUrl($this, $name, $parameters, $absolute);
+//    }
+//
+//
+//
+//
+//
+//
+//
+//    /**
+//     * {@inheritdoc}
+//     */
+//    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+//    {
+//        //SF forms update hasn't been changed in sonata yet !
+////        $datagridMapper
+////            ->add('name')
+////            ->add('host')
+////        ;
+//    }
+//
+//
     /**
      * Configure form per locale
-     * 
-     * @param   Sonata\AdminBundle\Form\FormMapper $formMapper 
+     *
+     * @param   Sonata\AdminBundle\Form\FormMapper $formMapper
      * @return  void
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $locale = $this->getTranslatableLocale();
         $formMapper
             ->with($this->trans('form_site.label_general'))
-                ->add('name', null, array('help' => 'Vix te omnium sententiae.', 'label' => 'admin.name'))
-                ->add('host', null, array('label' => 'admin.host'))
-                ->add('relativePath', 'text', array('help' => 'Vero verear sapientem has at.', 'label' => 'admin.relativePath', 'required' => false))
-                ->add('isDefault', 'checkbox', array('label' => 'admin.isDefault', 'required' => false))
-                ->add('isActive', 'checkbox', array('label' => 'admin.isActive', 'required' => false))
-                
-                ->add('theme', 'choice', array('label' => 'admin.theme', 'choices' => $this->_themeManager->getAvailableThemeCodesForSelect()))
+                ->add('name', 'text', array('attr' => array('class' => 'sonata-medium locale locale_' . $locale), 'help' => 'Vix te omnium sententiae.', 'label' => 'admin.name'))
+                ->add('host', 'text', array('attr' => array('class' => 'sonata-medium locale locale_' . $locale), 'label' => 'admin.host'))
+                ->add('isDefault', 'checkbox', array('attr' => array('class' => 'locale locale_' . $locale), 'label' => 'admin.isDefault', 'required' => false))
+                ->add('isActive', 'checkbox', array('attr' => array('class' => 'locale locale_' . $locale), 'label' => 'admin.isActive', 'required' => false))
+
+                ->add('theme', 'choice', array('attr' => array('class' => 'sonata-medium locale locale_' . $locale), 'label' => 'admin.theme', 'choices' => $this->_themeManager->getAvailableThemeCodesForSelect()))
                 ->add('defaultLocale', 'choice', array('label' => 'admin.defaultLocale', 'choices' => $this->_availableLocales))
                 ->add('availableLocales', 'choice', array(
                     'label'     => 'admin.availableLocales',
                     'choices'   => $this->_availableLocales,
-                    'expanded'  => true, 
+                    'expanded'  => true,
                     'multiple'  => true
                 ))
             ->end()
@@ -159,59 +177,39 @@ class WebsiteAdmin extends Admin
     }
 
 
+
+
     /**
      * Allow to select locale to edit in side menu
-     * 
-     * @param   MenuItemInterface $menu 
-     * @param   $action 
+     *
+     * @param   MenuItemInterface $menu
+     * @param   $action
      * @param   Sonata\AdminBundle\Admin\Admin $childAdmin
      * @return  void
      */
-    protected function configureSideMenu(MenuItemInterface $menu, $action, Admin $childAdmin = null)
+    protected function configureSideMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null)
     {
-        if (!in_array($action, array('edit')) || is_null($this->getSubject()->getId())) {
+
+        $object = $this->getSubject();
+        if (!in_array($action, array('edit')) || is_null($this->getUrlsafeIdentifier($object))) {
             return;
         }
 
-        $id = $this->getSubject()->getId();
 
-        foreach ($this->getSubject()->getAvailableLocales() as $locale) {
+        foreach ($object->getAvailableLocales() as $locale) {
             $menuItem = $menu->addChild(
                 $this->trans($locale),
-                array('uri' => $this->generateUrl('edit', array('id' => $id, 'locale' => $locale)))
+                array('uri' => $this->generateObjectUrl('edit', $object, array('translatable_locale' => $locale)))
             );
-			$menuItem->setAttribute('class', 'locale-' . $locale);
+			$menuItem->setAttribute('class', 'locale locale_' . $locale);
 
-            // select current edited locale item in menu 
-            if ($this->getSubject()->getLocale() == $locale) {
-                $menu->setCurrentUri($this->generateUrl('edit', array('id' => $id, 'locale' => $locale)));
+            // select current edited locale item in menu
+            if ($object->getLocale() == $locale) {
+                $menu->setCurrentUri($this->generateObjectUrl('edit', $object, array('translatable_locale' => $locale)));
             }
         }
     }
 
 
-    /**
-     * Refresh object to load locale get in param
-     * 
-     * @param   $id
-     * @return  $subject
-     */
-    public function getObject($id)
-    {
-        $subject = parent::getObject($id);
 
-        // Get local in param then get current website default
-        if (!is_null($this->getRequest()->get('locale'))) {
-            $subject->setLocale($this->getRequest()->get('locale'));
-        } else {
-            $subject->setLocale($subject->getDefaultLocale());
-        }
-        
-        // Reload website data for selected locale
-        if ($subject->getTranslations()->count()) {
-            $this->getModelManager()->getEntityManager($this->getClass())->refresh($subject);
-        }
-
-        return $subject;
-    }
 }
---
+++ Document/Website.php
@@ -0,0 +1,237 @@
+<?php
+/*
+ * This file is part of the Presta Bundle project.
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\Document;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Presta\Common\Doctrine\Document\BaseTranslatableDocument;
+
+/**
+ * Website Document
+ *
+ * @package    Presta
+ * @subpackage CMSCoreBundle
+ * @author     Nicolas Bastien <nbastien@prestaconcept.net>
+ *
+ * @PHPCRODM\Document(translator="attribute")
+ */
+class Website extends BaseTranslatableDocument
+{
+    /**
+     * to create the document at the specified location. read only for existing documents.
+     *
+     * @PHPCRODM\Id
+     */
+    protected $path;
+
+    /**
+     * @PHPCRODM\Node
+     */
+    public $node;
+
+    /**
+     * @var string $host
+     * @PHPCRODM\String(translated=true)
+     */
+    protected $host;
+
+
+    /**
+     * @var string $theme
+     * @PHPCRODM\String(translated=true)
+     */
+    protected $theme;
+
+    /**
+     * @var string $name
+     * @Assert\NotBlank
+     * @PHPCRODM\String(translated=true)
+     */
+    protected $name;
+
+    /**
+     * @var boolean $is_active
+     * @PHPCRODM\Boolean(translated=true)
+     */
+    protected $is_active;
+
+    /**
+     * @var boolean $is_default
+     * @PHPCRODM\Boolean(translated=true)
+     */
+    protected $is_default;
+
+    /**
+     * @var string $default_locale
+     * @PHPCRODM\String()
+     */
+    protected $default_locale;
+
+    /**
+     * @var array $available_locales
+     * @PHPCRODM\String(multivalue=true)
+     */
+    protected $available_locales;
+
+    /**
+     * Used by Admin edition
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
+
+
+    /**
+     * @param array $available_locales
+     */
+    public function setAvailableLocales($available_locales)
+    {
+        $this->available_locales = $available_locales;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAvailableLocales()
+    {
+        return $this->available_locales;
+    }
+
+    /**
+     * @param string $default_locale
+     */
+    public function setDefaultLocale($default_locale)
+    {
+        $this->default_locale = $default_locale;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultLocale()
+    {
+        return $this->default_locale;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param boolean $is_active
+     */
+    public function setIsActive($is_active)
+    {
+        $this->is_active = $is_active;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->is_active;
+    }
+
+    /**
+     * @param boolean $is_default
+     */
+    public function setIsDefault($is_default)
+    {
+        $this->is_default = $is_default;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsDefault()
+    {
+        return $this->is_default;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        $this->path = '/website/' . $name; //todo!
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $theme
+     */
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+}
