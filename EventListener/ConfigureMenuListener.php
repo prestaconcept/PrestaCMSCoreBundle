101a8cd56e18cb646c7530e22ea5d478bb8ccc36
Nicolas Bastien <nbastien@prestaconcept.net>
2013-01-03 11:25:44 +0100

    CMS : refactoring menu

--- EventListener/ConfigureMenuListener.php
+++ EventListener/ConfigureMenuListener.php
@@ -38,32 +38,32 @@ class ConfigureMenuListener
         $translator = $this->container->get('translator');
         $menu = $event->getMenu();
 
-        $cmsMenuItem = $menu->addChild('CMS', array('route' => 'admin_presta_cmscore_website_list'));
-        $cmsMenuItem->setAttribute('class', 'dropdown');
-        $cmsMenuItem->setLabel($translator->trans('admin.cms').'<span class="caret"></span>');
-        $cmsMenuItem->setExtra('safe_label', true);
-        $cmsMenuItem->setLinkAttribute('class', 'dropdown-toggle  nav-menu nav-menu-cms');
-        $cmsMenuItem->setLinkAttribute('data-toggle', 'dropdown');
-        $cmsMenuItem->setChildrenAttribute('class', 'dropdown-menu');
-
-        $websiteItem = $cmsMenuItem->addChild('Websites', array('route' => 'admin_presta_cmscore_website_list'));
-		$websiteItem->setExtra('safe_label', true);
-		$websiteItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.websites'), $translator->trans('admin.websites.description')));
-		$websiteItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-websites');
-
-		$pageItem = $cmsMenuItem->addChild('Pages', array('route' => 'presta_cms_page'));
-		$pageItem->setExtra('safe_label', true);
-        $pageItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.pages'), $translator->trans('admin.pages.description')));
-		$pageItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-pages');
-
-        $themeItem = $cmsMenuItem->addChild('Themes', array('route' => 'presta_cms_theme'));
-		$themeItem->setExtra('safe_label', true);
-        $themeItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.themes'), $translator->trans('admin.themes.description')));
-		$themeItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-themes');
-
-		$dataItem = $cmsMenuItem->addChild('data', array('route' => 'presta_cms_data'));
-		$dataItem->setExtra('safe_label', true);
-		$dataItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.data'), $translator->trans('admin.data.description')));
-		$dataItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-data');
+//        $cmsMenuItem = $menu->addChild('CMS', array('route' => 'admin_presta_cmscore_website_list'));
+//        $cmsMenuItem->setAttribute('class', 'dropdown');
+//        $cmsMenuItem->setLabel($translator->trans('admin.cms').'<span class="caret"></span>');
+//        $cmsMenuItem->setExtra('safe_label', true);
+//        $cmsMenuItem->setLinkAttribute('class', 'dropdown-toggle  nav-menu nav-menu-cms');
+//        $cmsMenuItem->setLinkAttribute('data-toggle', 'dropdown');
+//        $cmsMenuItem->setChildrenAttribute('class', 'dropdown-menu');
+//
+//        $websiteItem = $cmsMenuItem->addChild('Websites', array('route' => 'admin_presta_cmscore_website_list'));
+//		$websiteItem->setExtra('safe_label', true);
+//		$websiteItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.websites'), $translator->trans('admin.websites.description')));
+//		$websiteItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-websites');
+//
+//		$pageItem = $cmsMenuItem->addChild('Pages', array('route' => 'presta_cms_page'));
+//		$pageItem->setExtra('safe_label', true);
+//        $pageItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.pages'), $translator->trans('admin.pages.description')));
+//		$pageItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-pages');
+//
+//        $themeItem = $cmsMenuItem->addChild('Themes', array('route' => 'presta_cms_theme'));
+//		$themeItem->setExtra('safe_label', true);
+//        $themeItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.themes'), $translator->trans('admin.themes.description')));
+//		$themeItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-themes');
+//
+//		$dataItem = $cmsMenuItem->addChild('data', array('route' => 'presta_cms_data'));
+//		$dataItem->setExtra('safe_label', true);
+//		$dataItem->setLabel(sprintf('%s <p>%s</p>', $translator->trans('admin.data'), $translator->trans('admin.data.description')));
+//		$dataItem->setLinkAttribute('class', 'nav-submenu nav-submenu-cms-data');
     }
 }
