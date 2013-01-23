bfff410c81ebc27f9fbe952448b92a8d0e7e5ae0
Nicolas Bastien <nbastien@prestaconcept.net>
2013-01-23 12:05:03 +0100

    CMS: remove old comment + CS

--- Model/PageManager.php
+++ Model/PageManager.php
@@ -29,12 +29,12 @@ class PageManager
     /**
      * @var Symfony\Component\DependencyInjection\Container
      */
-    protected $_container;
+    protected $container;
     
     /**
      * @var Presta\CMSCoreBundle\Repository\PageRepository
      */
-    protected $_repository;
+    protected $repository;
 
     /**
      * @var Page
@@ -44,14 +44,14 @@ class PageManager
     /**
      * @var array 
      */
-    protected $_types;
+    protected $types;
 
     public function __construct($container)
     {
-        $this->_container = $container;
-        $this->_websites = null;
-        $this->_repository = null;
-        $this->_types = array(
+        $this->container = $container;
+        $this->websites = null;
+        $this->repository = null;
+        $this->types = array(
             //todo inject via config
             'cms_page' => 'presta_cms.page_type.cms_page',
         );
@@ -62,7 +62,7 @@ class PageManager
      */
     public function getDocumentManager()
     {
-        return $this->_container->get('doctrine_phpcr.odm.default_document_manager');
+        return $this->container->get('doctrine_phpcr')->getManager();
     }
 
     /**
@@ -70,13 +70,12 @@ class PageManager
      * 
      * @return
      */
-    protected function _getRepository()
+    protected function getRepository()
     {
-        if ($this->_repository == null) {
-            $this->_repository =$this->_container->get('doctrine_phpcr.odm.default_document_manager')
-                ->getRepository('Presta\CMSCoreBundle\Document\Page');
+        if ($this->repository == null) {
+            $this->repository = $this->getDocumentManager->getRepository('Presta\CMSCoreBundle\Document\Page');
         }
-        return $this->_repository;
+        return $this->repository;
     }
 
     /**
@@ -99,35 +98,7 @@ class PageManager
 
         return $page;
     }
-    
-//    /**
-//     * Return page by id
-//     *
-//     * @param  Application\Presta\CMSCoreBundle\Entity\Website $website
-//     * @param  integer $id
-//     * @return Application\Presta\CMSCoreBundle\Entity\Page
-//     */
-//    public function getPageById(Website $website, $id)
-//    {
-//        return $this->_getRepository()->getPageById($website, $id);
-//    }
-//
-//    /**
-//     * Return page by id
-//     *
-//     * @param  Application\Presta\CMSCoreBundle\Entity\Website $website
-//     * @param  string $url
-//     * @return Application\Presta\CMSCoreBundle\Entity\Page
-//     */
-//    public function getPageByUrl(Website $website, $url)
-//    {
-//		//todo handle first '/' ?
-//		if (strpos($url, '/') === 0) {
-//			$url = substr($url, 1);
-//		}
-//		return $this->_getRepository()->getPageByUrl($website, $url);
-//    }
-    
+
     /**
      * Update page
      *
@@ -148,109 +119,6 @@ class PageManager
         $this->getDocumentManager()->flush();
     }
     
-//    /**
-//     * Return root page node for a navigation
-//     *
-//     * @param  Website $website
-//     * @param  string $navigation
-//     * @return Page
-//     */
-//    public function getNavigationRoot(Website $website, $navigation)
-//    {
-//        return $this->_getRepository()->getNavigationRoot($website, $navigation);
-//    }
-    
-//    /**
-//     * Return pages from navigation
-//     *
-//     * @param  Application\Presta\CMSCoreBundle\Entity\Website $website
-//     * @param  string $navigation
-//     * @param  boolean $direct - true to take only direct children
-//     * @return ArrayCollection
-//     */
-//    public function getNavigationPages(Website $website, $navigation, $direct = false)
-//    {
-//        $root = $this->getNavigationRoot($website, $navigation);
-//        if ($root == null) {
-//			return array();
-//        }
-//        return $this->_getRepository()->getChildrenPages($website, $root, $direct);
-//    }
-//
-//    /**
-//     * Return navigation tree HTML code
-//     *
-//     * @param  Website $website
-//     * @param  string $navigation
-//	 * @param  boolean $withCheckboxes
-//     * @return string
-//     */
-//    public function getNavigationTree(Website $website, $navigation, $withCheckboxes = false)
-//    {
-//        $nodes = $this->getNavigationPages($website, $navigation);
-//        return $this->buildTree($nodes, $website, $withCheckboxes);
-//    }
-//
-//    /**
-//     * Return single pages
-//     *
-//     * @param  Application\Presta\CMSCoreBundle\Entity\Website $website
-//     * @return ArrayCollection
-//     */
-//    public function getSinglePages(Website $website)
-//    {
-//        return $this->_getRepository()->getSinglePages($website);
-//    }
-    
-//    /**
-//     * Return single pages tree HTML code
-//     *
-//     * @param  Website $website
-//	 * @param  boolean $withCheckboxes
-//     * @return string
-//     */
-//    public function getSinglePagesTree(Website $website, $withCheckboxes = false)
-//    {
-//        $nodes = $this->getSinglePages($website);
-//        return $this->buildTree($nodes, $website, $withCheckboxes);
-//    }
-    
-//    /**
-//     * Return tree HTML code for a node list
-//     *
-//     * @param  array $nodes
-//     * @param  Website $website
-//	 * @param  boolean $withCheckboxes
-//     * @return string
-//     */
-//    public function buildTree(array $nodes, Website $website, $withCheckboxes = false)
-//    {
-//        //Generate edit url foreach pages
-//        foreach ($nodes as &$node) {
-//            $node['edit_url'] = $this->_container->get('router')->generate('presta_cms_page_edit', array(
-//                'id' => $node['id'],
-//                'website_id' => $website->getId(),
-//                'locale' => $website->getLocale()
-//            ));
-//			$node['withCheckboxes'] = $withCheckboxes;
-//        }
-//        $options = array(
-//            'decorate' => true,
-//            'rootOpen' => '<ul>',
-//            'rootClose' => '</ul>',
-//            'childOpen' => '<li id="page_',
-//            'childClose' => '</li>',
-//            'nodeDecorator' => function($node) {
-//				if ($node['withCheckboxes']) {
-//					return $node['id'] . '"><input type="checkbox" name="cms_page_tree" value="' . $node['id'] . '"/>' . $node['name'] . '';
-//				}
-//                return $node['id'] . '"><a href="' . $node['edit_url'] . '">' . $node['name'] . '</a>';
-//            }
-//        );
-//
-//        return $this->_getRepository()->buildTree($nodes, $options);
-//    }
-    
     /**
      * Reference a new page type service
      * 
@@ -260,20 +128,20 @@ class PageManager
      */
     public function addType($idType, $typeServiceId)
     {
-        $this->_types[$idType] = $typeServiceId;
+        $this->types[$idType] = $typeServiceId;
         return $this;
     }
     
     /**
-     * Return correspondign page type service
+     * Return corresponding page type service
      * 
      * @param  string $idType
      * @return false|Presta\CMSCoreBundle\Model\PagePageTypeInterface
      */
     public function getType($idType)
     {
-        if (isset($this->_types[$idType])) {
-            return $this->_container->get($this->_types[$idType]);
+        if (isset($this->types[$idType])) {
+            return $this->container->get($this->types[$idType]);
         }
         return false;  
     }
@@ -293,6 +161,5 @@ class PageManager
     {
         return $this->currentPage;
     }
-
 
 }
