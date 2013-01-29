52c2919b9b9a2ff3a50ecc605aa9fdef11a53cf6
Nicolas Bastien <nbastien@prestaconcept.net>
2013-01-29 15:40:21 +0100

    Block test

---
+++ Tests/Unit/Document/BlockTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the Presta Bundle project.
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\Tests\Unit\Model;
+
+use Presta\CMSCoreBundle\Tests\Unit\BaseUnitTestCase;
+
+use Presta\CMSCoreBundle\Document\Block;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class BlockTest extends BaseUnitTestCase
+{
+    public function testBlock()
+    {
+        $block = new Block();
+
+        $block->setType('presta_cms.block.simple');
+        $this->assertEquals('presta_cms.block.simple', $block->getType());
+
+        $this->assertEquals(false, $block->isAdminMode());
+        $block->setAdminMode();
+        $this->assertEquals(true, $block->isAdminMode());
+
+        $this->assertEquals(false, $block->isEditable());
+        $block->setIsEditable(true);
+        $this->assertEquals(true, $block->isEditable());
+
+        $this->assertEquals(false, $block->isDeletable());
+        $block->setIsDeletable(true);
+        $this->assertEquals(true, $block->isDeletable());
+
+        $block->setLocale('en');
+        $this->assertEquals('en', $block->getLocale());
+
+        $this->assertEquals(array(), $block->getSettings());
+        $block->setSetting('is_awesome', true);
+        $this->assertEquals(array('is_awesome' => true), $block->getSettings());
+
+        $this->assertEquals(true, $block->isActive());
+        $block->setIsActive(false);
+        $this->assertEquals(false, $block->isActive());
+
+        $block->setId('/website/default/.._..__/block-id');
+        $this->assertEquals('websitedefaultblock-id', $block->getHtmlId());
+    }
+
+}
+
