88206d79fef58843e1b5b45706eacd6c28730c1e
Nicolas Bastien <nbastien@prestaconcept.net>
2013-02-08 14:07:45 +0100

    Issue #7
    Base fixture class

---
+++ DataFixtures/PHPCR/BaseFixture.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the Presta Bundle project.
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\DataFixtures\PHPCR;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+/**
+ * Base fixtures methods
+ */
+abstract class BaseFixture extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    abstract function getOrder();
+
+    /**
+     * Returns available locale to initialise documents
+     *
+     * @return array
+     */
+    protected function getLocales()
+    {
+        return $this->container->getParameter('locales');
+    }
+}
