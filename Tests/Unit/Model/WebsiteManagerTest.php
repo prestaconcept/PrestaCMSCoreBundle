b7957a7536f050e9f5ecceacabf9ff78a80f292e
Alain Flaus <aflaus@prestaconcept.net>
2013-04-24 12:12:58 +0200

    Remove test

--- Tests/Unit/Model/WebsiteManagerTest.php
+++ Tests/Unit/Model/WebsiteManagerTest.php
@@ -91,14 +91,4 @@ class WebsiteManagerTest extends BaseUnitTestCase
 
         //@next see to add exception on malformed configuration
     }
-
-    public function testSetCurrentWebsite()
-    {
-        $websiteManager = $this->getWebsiteManager();
-        $this->assertEquals(null, $websiteManager->getCurrentWebsite());
-
-        // @todo: find an other way to test setCurrentWebsite()
-        // $websiteManager->setCurrentWebsite(null);
-        // $this->assertEquals(null, $websiteManager->getCurrentWebsite());
-    }
 }
