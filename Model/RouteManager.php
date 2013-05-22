3e3ca670b04b429cff974fed960316a6d48291cb
David EPELY <depely@prestaconcept.net>
2013-05-22 14:51:55 +0200

    add routes for hierarchy in route manager
    + fix cs
    + fix missing parenthesis in page manager

--- Model/RouteManager.php
+++ Model/RouteManager.php
@@ -1,10 +1,15 @@
 <?php
-
+/**
+ * This file is part of the Presta Bundle project.
+ *
+ * @author David Epely <depely@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Presta\CMSCoreBundle\Model;
 
 use Symfony\Cmf\Component\Routing\RouteProviderInterface;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Cmf\Bundle\RoutingExtraBundle\Document\Route;
 
@@ -13,27 +18,59 @@ use Presta\CMSCoreBundle\Document\Website;
 /**
  * Description of RouteManager
  *
+ * @package    PrestaCMS
+ * @subpackage CoreBundle
  * @author David Epely <depely@prestaconcept.net>
  */
-class RouteManager 
+class RouteManager
 {
+    /**
+     * @var RouteProviderInterface
+     */
     protected $routeProvider;
-    
+
     public function __construct(RouteProviderInterface $routeProvider)
     {
         $this->routeProvider = $routeProvider;
     }
-    
-    
+
     /**
-     * @param \Presta\CMSCoreBundle\Document\Website $website
+     * @param  \Presta\CMSCoreBundle\Document\Website $website
      * @return RouteCollection
      */
     public function findRoutesByWebsite(Website $website)
     {
         //Locale is in host only; then we list children for current locale
-        $baseRoute = $this->routeProvider->getRouteByName($website->getPath() . '/route/' . $website->getLocale());
-        return $this->routeProvider->getRouteCollectionForHierarchy($baseRoute);
+        $baseRoute = $this->routeProvider->getRouteByName($website->getRoutePrefix());
+
+        if (!$baseRoute) {
+            throw new \RuntimeException('Website must has a route');
+        }
+
+        return $this->getRouteCollectionForHierarchy($baseRoute);
     }
-    
+
+    /**
+     * get routes recursively
+     *
+     * @param  Route           $route
+     * @return RouteCollection
+     */
+    public function getRouteCollectionForHierarchy(Route $route)
+    {
+        $routeCollection = new RouteCollection();
+
+        // SYMFONY 2.1 COMPATIBILITY: tweak route name
+        $routeName = trim(preg_replace('/[^a-z0-9A-Z_.]/', '_', $route->getRouteKey()), '_');
+        $routeCollection->add($routeName, $route);
+
+        foreach ($route->getRouteChildren() as $child) {
+            //route cannot be other than RouteObjectInterface
+            if ($child instanceof Route) {
+                $routeCollection->addCollection($this->getRouteCollectionForHierarchy($child));
+            }
+        }
+
+        return $routeCollection;
+    }
 }
