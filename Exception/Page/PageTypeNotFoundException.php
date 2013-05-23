84de7f4af0499fa9a9f188f544383dd825112660
Nicolas Bastien <NikoR6@gmail.com>
2013-05-23 18:24:01 +0200

    #57 Implement a dynamic way to handle page types
    load pages type like the blocks : add a new service tag and a compiler pass
    rework the types id to fit to the service one
    rework phpdocs
    rework menu item in menu nodes to follow CMF changes

---
+++ Exception/Page/PageTypeNotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Presta Bundle project.
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\Exception\Page;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PageTypeNotFoundException extends \InvalidArgumentException
+{
+    public function __construct($pageTypeId)
+    {
+        $this->message = 'Page type not found : ' . $pageTypeId
+            . PHP_EOL . 'Maybe you should install the corresponding bundle';
+    }
+}
