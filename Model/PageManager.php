b4813642eab5436faa582b511f3b76586b80f485
Alain Flaus <aflaus@prestaconcept.net>
2013-05-24 17:16:01 +0200

    Issue #10: Plug routing update on page edition

--- Model/PageManager.php
+++ Model/PageManager.php
@@ -13,6 +13,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\PHPCR\DocumentRepository;
 use Presta\CMSCoreBundle\Event\PageDeletionEvent;
 use Presta\CMSCoreBundle\Exception\Page\PageTypeNotFoundException;
+use Presta\CMSCoreBundle\Event\PageUpdateEvent;
+use Symfony\Cmf\Bundle\MenuBundle\Document\MenuItem;
 use Presta\CMSCoreBundle\Document\Page;
 use Doctrine\ODM\PHPCR\DocumentManager;
 use Presta\CMSCoreBundle\Model\Page\PageTypeInterface;
@@ -111,17 +113,13 @@ class PageManager
      */
     public function update($page)
     {
-        //Update corresponding route
-        $route = $page->getRoute();
-        $route->setName($page->getUrl());
-
-        //todo voir pour faire mieux :
-        //création d'une redirection pour l'ancien url
-        //pas de modification de la home sinon ça case le prefix des routes!
-
-        $this->getDocumentManager()->persist($route);
         $this->getDocumentManager()->persist($page);
         $this->getDocumentManager()->flush();
+
+        $this->container->get('event_dispatcher')->dispatch(
+            PageUpdateEvent::EVENT_NAME,
+            new PageUpdateEvent($page)
+        );
     }
 
     /**
@@ -216,20 +214,5 @@ class PageManager
     public function isValidToken($page, $token)
     {
         return ($token == $this->getToken($page));
-    }
-
-    /**
-     * Return page full url
-     *
-     * @param  Page $page
-     * @return string
-     */
-    public function getPageUrl($page)
-    {
-        return str_replace(
-            $page->getRoute()->getPrefix(),
-            '',
-            $page->getRoute()->getId()
-        );
     }
 }
