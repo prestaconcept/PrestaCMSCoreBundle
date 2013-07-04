d65211ef827cfaa95005c0f883e424821cb13124
Alain Flaus <aflaus@prestaconcept.net>
2013-07-04 15:55:21 +0200

    Retrieve only Page node in getChildren method

--- Tree/PageTree.php
+++ Tree/PageTree.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Presta Bundle project.
  *
@@ -12,12 +11,85 @@ namespace Presta\CMSCoreBundle\Tree;
 
 use Symfony\Cmf\Bundle\TreeBrowserBundle\Tree\PHPCRTree;
 
+use Doctrine\Bundle\PHPCRBundle\ManagerRegistry;
+
+use PHPCR\Util\NodeHelper;
+use PHPCR\PropertyType;
+
+/**
+ * PrestaCMS page tree
+ */
 class PageTree extends PHPCRTree
 {
     /**
-     * Get the alias for this tree
-     *
-     * @return string
+     * @var \PHPCR\SessionInterface
+     */
+    private $session;
+
+    /**
+     * Constructor
+     * 
+     * @param ManagerRegistry $manager     
+     * @param string          $sessionName
+     */
+    public function __construct(ManagerRegistry $manager, $sessionName)
+    {
+        $this->session = $manager->getConnection($sessionName);
+    }
+
+    /**
+     * Return list of page type node
+     * 
+     * @param  string $path
+     * @return array
+     */
+    public function getChildren($path)
+    {
+        $root = $this->session->getNode($path);
+
+        $children = array();
+
+        foreach ($root->getNodes() as $name => $node) {
+            // keep only Page node
+            if ($node->getPropertyValue('phpcr:class') == 'Presta\CMSCoreBundle\Document\Page') {
+                if (NodeHelper::isSystemItem($node)) {
+                    continue;
+                }
+                $child = $this->nodeToArray($name, $node);
+
+                foreach ($node->getNodes() as $childname => $grandson) {
+                    // keep only Page node
+                    if ($grandson->getPropertyValue('phpcr:class') == 'Presta\CMSCoreBundle\Document\Page') {
+                        $child['children'][] = $this->nodeToArray($childname, $grandson);
+                    }
+                }
+
+                $children[] = $child;
+            }
+        }
+
+        return $children;
+    }
+
+    /**
+     * { @inherit }
+     */
+    private function nodeToArray($name, $node)
+    {
+        $has_children = $node->hasNodes();
+        return array(
+            'data'  => $name,
+            'attr'  => array(
+                'id' => $node->getPath(),
+                'url_safe_id' => substr($node->getPath(), 1),
+                'rel' => 'node'
+            ),
+            'state' => $has_children ? 'closed' : null,
+        );
+    }
+
+    /**
+     * { @inherit }
      */
     public function getAlias()
     {
