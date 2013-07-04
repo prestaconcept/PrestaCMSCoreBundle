e8130997f5336cadb7191f977462a90625609fb0
Alain Flaus <aflaus@prestaconcept.net>
2013-07-04 12:01:47 +0200

    FormType for presta_cms page tree

---
+++ Form/Type/PageTreeModelType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the Presta Bundle project.
+ *
+ * (c) Alain Flaus <aflaus@prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Cmf\Bundle\TreeBrowserBundle\Tree\TreeInterface;
+
+/**
+ * 
+ */
+class PageTreeModelType extends AbstractType
+{
+    protected $defaults = array();
+    protected $tree;
+
+    public function setDefaults(array $defaults)
+    {
+        $this->defaults = $defaults;
+    }
+
+    public function setTree(TreeInterface $tree)
+    {
+        var_dump(get_class($tree));
+        $this->tree = $tree;
+    }
+
+    public function getParent()
+    {
+        return 'doctrine_phpcr_odm_tree';
+    }
+
+    public function getName()
+    {
+        return 'presta_cms_doctrine_phpcr_odm_page_tree';
+    }
+}
