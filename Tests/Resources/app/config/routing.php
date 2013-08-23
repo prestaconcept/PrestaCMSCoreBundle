508ae8bfaf9ab1f49ce7d4fa97045526324316f0
David Epely <depely@prestaconcept.net>
2013-08-23 12:56:44 +0200

    configure behat and travis using cmf testing component

--- Tests/Resources/app/config/routing.php
+++ Tests/Resources/app/config/routing.php
@@ -5,11 +5,12 @@
 use Symfony\Component\Routing\RouteCollection;
 
 $collection = new RouteCollection();
+
+$collection->addCollection(
+    $loader->import(__DIR__.'/routing/prestacmscore_routing.yml')
+);
 $collection->addCollection(
     $loader->import(CMF_TEST_CONFIG_DIR.'/routing/sonata_routing.yml')
 );
-//$collection->addCollection(
-//    $loader->import(__DIR__.'/routing/test.yml')
-//);
 
 return $collection;
