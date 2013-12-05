b8caad7108bbb9cf3a98ca10923877e25b35c0d4
Nicolas Bastien <nbastien@prestaconcept.net>
2013-12-05 15:01:38 +0100

    #126 : Handle custom page with specific controller
    Create an abstract controller which will be the base for every custom controller.
    Routing will be done with standard Symfony routing

---
+++ Controller/AbstractController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of the PrestaCMSCoreBundle.
+ *
+ * (c) PrestaConcept <http://www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Presta\CMSCoreBundle\Model\ThemeManager;
+use Presta\CMSCoreBundle\Model\WebsiteManager;
+use Sonata\SeoBundle\Seo\SeoPageInterface;
+
+/**
+ * This base controller handle the loading of all the data needed by PrestaCMS layout
+ *
+ * You should extend it if you want to add a custom controller to your PrestaCMS application
+ * A good example can be found in PrestaCMSUserBundle
+ *
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class AbstractController extends Controller
+{
+    /**
+     * @return WebsiteManager
+     */
+    protected function getWebsiteManager()
+    {
+        return $this->get('presta_cms.manager.website');
+    }
+
+    /**
+     * @return ThemeManager
+     */
+    protected function getThemeManager()
+    {
+        return $this->get('presta_cms.manager.theme');
+    }
+
+    /**
+     * @return SeoPageInterface
+     */
+    protected function getSeoManager()
+    {
+        return $this->get('sonata.seo.page');
+    }
+
+    /**
+     * Returns all the necessary view params needed to render the theme layout
+     *
+     * @return array
+     */
+    protected function getBaseViewParams()
+    {
+        $website    = $this->getWebsiteManager()->getCurrentWebsite();
+        $theme      = $this->getThemeManager()->getTheme($website->getTheme(), $website);
+
+        return array(
+            'base_template'     => $theme->getTemplate(),
+            'website'           => $website,
+            'websiteManager'    => $this->getWebsiteManager(),
+            'theme'             => $theme
+        );
+    }
+}
